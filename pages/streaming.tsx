56920efbe877fe4790880c4e5c971978bc54bce7
issy20 <gggibraltar86@gmail.com>
2021-12-26 13:39:29 +0900

    edit style

--- pages/streaming.tsx
+++ pages/streaming.tsx
@@ -28,53 +28,51 @@ const Streaming: NextPage = () => {
 
   return (
     <Layout>
-      <div className=" bg-white shadow-2xl text-center rounded text-xs py-16 px-8 md:w-96">
-        {_playingData?.is_playing && (
-          <>
-            <p className="font-semibold">Now Playing</p>
-            <br />
-            <a href={_playingData.item.uri}>
-              <Image
-                src={_playingData.item.album.images[2].url}
-                width={_playingData.item.album.images[2].width}
-                height={_playingData.item.album.images[2].height}
-                alt="jacket"
-              />
-            </a>
+      {_playingData?.is_playing && (
+        <>
+          <p className="font-semibold">Now Playing</p>
+          <br />
+          <a href={_playingData.item.uri}>
+            <Image
+              src={_playingData.item.album.images[2].url}
+              width={_playingData.item.album.images[2].width}
+              height={_playingData.item.album.images[2].height}
+              alt="jacket"
+            />
+          </a>
 
-            <p className="mt-2">{_playingData.item.artists[0].name}</p>
-            <p>{_playingData.item.name}</p>
-            <br />
-            <br />
-            <Link href="/home">Back to Home</Link>
-          </>
-        )}
-        {!_playingData?.is_playing && _playedData?.track.id && (
-          <>
-            <p className="mt-2 font-semibold">Last Played</p>
-            <br />
-            <a href={_playedData.track.uri}>
-              <Image
-                src={_playedData.track.album.images[2].url}
-                width={_playedData.track.album.images[2].width}
-                height={_playedData.track.album.images[2].height}
-                alt="jacket"
-              />
-            </a>
-            <br />
-            <p className="mt-2">
-              {_playedData.track.artists.map((artist) => artist.name)}
-            </p>
-            <p>{_playedData.track.name}</p>
-            <br />
-            <p>{_playedData.played_at}</p>
-            <br />
-            <br />
+          <p className="mt-2">{_playingData.item.artists[0].name}</p>
+          <p>{_playingData.item.name}</p>
+          <br />
+          <br />
+          <Link href="/home">Back to Home</Link>
+        </>
+      )}
+      {!_playingData?.is_playing && _playedData?.track.id && (
+        <>
+          <p className="mt-2 font-semibold">Last Played</p>
+          <br />
+          <a href={_playedData.track.uri}>
+            <Image
+              src={_playedData.track.album.images[2].url}
+              width={_playedData.track.album.images[2].width}
+              height={_playedData.track.album.images[2].height}
+              alt="jacket"
+            />
+          </a>
+          <br />
+          <p className="mt-2">
+            {_playedData.track.artists.map((artist) => artist.name)}
+          </p>
+          <p>{_playedData.track.name}</p>
+          <br />
+          <p>{_playedData.played_at}</p>
+          <br />
+          <br />
 
-            <Link href="/home">Back to Home</Link>
-          </>
-        )}
-      </div>
+          <Link href="/home">Back to Home</Link>
+        </>
+      )}
     </Layout>
   )
 }
